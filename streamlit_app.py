02f9b5bc4990c3c280469808c8d1408c2c632c9a
abdurrahman05110-lang <abdur.rahman05110@gmail.com>
2025-12-11 15:41:04 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
 
-number = st.number_input("Insert a number")
+number = st.number_input("Insert a number",min_value=0, max_value=100)
 st.write("The current number is ", number)
+if number
