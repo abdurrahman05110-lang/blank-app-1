466ce66e62cf291a042655a238196d2b84dd7c06
abdurrahman05110-lang <abdur.rahman05110@gmail.com>
2025-12-11 15:48:01 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
+st.title(":blue[Projek LPK 2025] :sunglasses:")
 number = st.number_input("Insert a number",min_value=0, max_value=1000)
 st.write("The current number is ", number)
 if number%2==1:
